052a60a7f752b78268ad47657ea205f7df8b92ee
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-04-15 19:19:24 +0530

    CheckSortedArray

---
+++ ARRAYS/CheckSortedArray.java
@@ -0,0 +1,18 @@
+
+
+public class CheckSortedArray {
+  ublic static int isSorted(int n, int []a) {
+    int i = 0;
+    int j=n-1;
+
+    while(i<n/2 && j >=n/2){
+        if(a[i] <= a[j]){
+            i++;
+            j--;
+        } else{
+            return 0;
+        }
+    }
+    return 1;
+ }
+}

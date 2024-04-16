9742cbe8838646aa34facc2cb2d86d0e8aaba7e8
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-04-17 00:53:02 +0530

    283. Move Zeroes

--- LEETCODE/283. Move Zeroes/Solution.java
+++ LEETCODE/283. Move Zeroes/Solution.java
@@ -1,60 +1,39 @@
-class Solution {
-    public int trap(int[] height) {
-      //optimal
-     
-        int l = 0; 
-        int r = height.length - 1;
-        int sum = 0;
-        int leftBar = height[l];
-        int  rightBar = height[r];
-        
-        while (l < r) {
-            if (leftBar <= rightBar) {
-                sum += leftBar - height[l];
-                l++;
-                leftBar = Math.max(leftBar, height[l]);
-            } else {
-                sum += rightBar - height[r];
-                r--;
-                rightBar = Math.max(rightBar, height[r]);
-            }
-        }
+//My Solution
 
-        return sum;
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int n = nums.length;
+        int[] arr = new int[n];
+         int i=0;
+      int j=0;
+       for(;i<n;i++){
+       if(nums[i] != 0){
+         arr[j] = nums[i];
+         j++;
+       }else{
+        continue;
+       }
+     }
+      for(int k=0;k<n;k++){
+   
+            nums[k] = arr[k];
+        }
     }
 }
-      
-      
-      
-      //Brute force
 
 
-        //       int maxheight = 0;
-
-        // for(int i = 1; i < height.length - 1; i++){ // Changed the loop condition to exclude the first and last elements
-        //     int ht = height[i];
-        //     int leftmax = 0; // Initialized leftmax and rightmax
-        //     int rightmax = 0;
-        //     int left = i - 1;
-        //     int right = i + 1;
-            
-        //     // Finding left max height
-        //     while(left >= 0){
-        //         if(height[left] > leftmax){
-        //             leftmax = height[left];
-        //         }
-        //         left--;
-        //     }
-
-        //     // Finding right max height
-        //     while(right < height.length){
-        //         if(height[right] > rightmax){
-        //             rightmax = height[right];
-        //         }
-        //         right++;
-        //     }
-
-        //     // Calculating the trapped water at the current index
-        //     maxheight += Math.max(0, Math.min(leftmax, rightmax) - ht);
-        // }
-        // return maxheight;
+// class Solution {
+//      public void moveZeroes(int[] nums) {
+//         int snowBallSize = 0; 
+//         for (int i=0;i<nums.length;i++){
+// 	        if (nums[i]==0){
+//                 snowBallSize++; 
+//             }
+//             else if (snowBallSize > 0) {
+// 	            int t = nums[i];
+// 	            nums[i]=0;
+// 	            nums[i-snowBallSize]=t;
+//             }
+//         }
+//     }
+// }

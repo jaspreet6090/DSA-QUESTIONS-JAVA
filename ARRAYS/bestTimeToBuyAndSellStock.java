a016a1163ef25592bfd7088477c05fe4829965c3
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-04-27 00:33:32 +0530

    bestTimeToBuyAndSellStock  - CN

---
+++ ARRAYS/bestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+public class bestTimeToBuyAndSellStock {
+  public static int bestTimeToBuyAndSellStock(int []prices) {
+
+    int mini = prices[0];
+    int n = prices.length;
+    int cost=0;
+    int profit =0;
+
+    for(int i =1 ; i < n ; i++){
+        cost = prices[i] - mini;
+        profit = Math.max(profit,cost);
+        mini = Math.min(mini , prices[i]);
+    }
+    return profit;
+
+}
+}

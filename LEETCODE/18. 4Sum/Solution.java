45f99f22da84563f15e37e14d9f3a6af408d2752
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-05-11 20:10:15 +0530

    Majority Element

--- LEETCODE/18. 4Sum/Solution.java
+++ LEETCODE/18. 4Sum/Solution.java
@@ -1,40 +1,39 @@
-
 class Solution {
   public List<List<Integer>> fourSum(int[] nums, int target) {
-       List<List<Integer>> res = new ArrayList<>();
-      Arrays.sort(nums);
-      HashSet<String> unique = new HashSet<>();
+     List<List<Integer>> res = new ArrayList<>();
+    Arrays.sort(nums);
+    HashSet<String> unique = new HashSet<>();
 
-      for (int i = 0; i < nums.length - 3; i++) {
-          for (int j = i + 1; j < nums.length - 2; j++) {
-              int left = j + 1;
-              int right = nums.length - 1;
+    for (int i = 0; i < nums.length - 3; i++) {
+      for (int j = i + 1; j < nums.length - 2; j++) {
+        int left = j + 1;
+        int right = nums.length - 1;
 
-              while (left < right) {
-                   long sum = nums[i];
-                   sum += nums[j];
-                  sum+= nums[left];
-              sum+= nums[right];
-                  if (sum < target) {
-                      left++;
-                  } else if (sum > target) {
-                      right--;
-                  } else {
-                      StringBuilder sb = new StringBuilder();
-                      sb.append(nums[i]).append(nums[j]).append(nums[left]).append(nums[right]);
-                      String code = sb.toString();
-                      if (!unique.contains(code)) {
-                          unique.add(code);
-                          res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-                      }
-                      left++;
-                      right--;
-                  }
-              }
+        while (left < right) {
+           long sum = nums[i];
+           sum += nums[j];
+          sum+= nums[left];
+        sum+= nums[right];
+          if (sum < target) {
+            left++;
+          } else if (sum > target) {
+            right--;
+          } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append(nums[i]).append(nums[j]).append(nums[left]).append(nums[right]);
+            String code = sb.toString();
+            if (!unique.contains(code)) {
+              unique.add(code);
+              res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+            }
+            left++;
+            right--;
           }
+        }
       }
-      return res;
     }
+    return res;
+  }
 }
   //Brute Force
   //    List<List<Integer>> result = new ArrayList<>();

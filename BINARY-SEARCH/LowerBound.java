30225971e352256326f27bcbc49546d5a9794510
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-05-23 19:15:16 +0530

    LowerBound

--- BINARY-SEARCH/LowerBound.java
+++ BINARY-SEARCH/LowerBound.java
@@ -1,6 +1,6 @@
 // Given a sorted array arr[] of size N without duplicates, and given a value x. Floor of x is defined as the largest element K in arr[] such that K is smaller than or equal to x. Find the index of K(0-based indexing).
 
-class Solution{
+class LowerBound{
     
   // Function to find floor of x
   // arr: input array

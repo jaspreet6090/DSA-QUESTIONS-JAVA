c205a7e548ee4e00919350aee6999b2b39da5205
Jaspreet Singh <jaspreetsingh6090@gmail.com>
2024-06-03 00:34:50 +0530

    linkedlist

---
+++ LINKED-LIST/linkedlist.java
@@ -0,0 +1,43 @@
+
+
+//  Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ };
+
+
+
+ public class linkedlist {
+  public static Node constructLL(int []arr) {
+    Node head = new Node(arr[0],null);
+  Node mover = head;
+
+  for(int i=1;i<arr.length;i++){
+      Node temp = new Node (arr[i]);
+      mover.next = temp;
+      mover = temp;
+  }
+
+  return head;
+  }
+}
